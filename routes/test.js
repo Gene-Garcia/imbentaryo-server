0358b9bb6fff44406afbf959d16ba4b93de79829
gn-grc <genejogarcia.gg@gmail.com>
2022-05-08 14:52:07 +0800

    test codes on database operations

--- routes/test.js
+++ routes/test.js
@@ -2,6 +2,7 @@
 const router = require("express").Router();
 
 const ctx = require("../database/databaseContext");
+const { generateKey, validateKey } = require("../utils/keyGenerator");
 
 router.get("/db", async (req, res) => {
   //   ctx.test((err, rows) => {
@@ -18,18 +19,23 @@ router.get("/db", async (req, res) => {
 
   //   console.log("out");
 
-  try {
-    const result = await ctx.runSelectOne(
-      "SELECT * FROM lorem WHERE info = (?)",
-      ["Ipsum 0"]
-    );
-    console.log("sending...");
-    return res.status(200).json(result);
-  } catch (error) {
-    console.log("error sa labas");
-    console.error(error);
-    return res.status(200).json(error);
-  }
+  // try {
+  //   const result = await ctx.runSelectOne(
+  //     "SELECT * FROM lorem WHERE info = (?)",
+  //     ["Ipsum 0"]
+  //   );
+  //   console.log("sending...");
+  //   return res.status(200).json(result);
+  // } catch (error) {
+  //   console.log("error sa labas");
+  //   console.error(error);
+  //   return res.status(200).json(error);
+  // }
+
+  const key = generateKey("Ipsum 0");
+  const result = await validateKey(key, "info", "Lorem");
+
+  return res.status(200).json({ key, result });
 });
 
 // export
