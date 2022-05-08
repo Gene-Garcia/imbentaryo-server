0de0d47c0553ec0533f45118b2211d1426e69176
gn-grc <genejogarcia.gg@gmail.com>
2022-05-08 14:49:06 +0800

    added new database context for multiple query

--- database/databaseContext.js
+++ database/databaseContext.js
@@ -1,6 +1,7 @@
 /*
  * All the database operations are wrapped in promises to allow
  * consuming code to await result instead of nesting callbacks
+ *
  */
 
 const sqlite3 = require("sqlite3");
@@ -22,6 +23,41 @@ exports.runQuery = (sql, params) => {
 
           resolve(true);
         });
+        stmt.finalize();
+      });
+
+      db.close();
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+/*
+ * arrayParams is a nested array. each row holds parameters
+ * intended for one row in the database
+ */
+exports.runMultipleQuery = (sql, arrayParams) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const db = new sqlite3.Database(fileLocation);
+
+      db.serialize(() => {
+        const stmt = db.prepare(sql);
+
+        arrayParams.forEach((params) => {
+          stmt.run(params, (err) => {
+            // during exeuction of 1 query an error was encountered
+            if (err) {
+              reject(err);
+              // return; IF WE STOP THE PROMISE THEN THE DATABASE WILL NOT BE CLOSED, https://deepscan.io/docs/rules/promise-reject-fall-through
+            }
+          });
+        });
+
+        // reaching here means that every insert was a success
+        resolve(true);
+
         stmt.finalize();
       });
 
