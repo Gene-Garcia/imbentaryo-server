f1719e83dff89ca6b15f3c83f874fbb8230f87a6
gn-grc <genejogarcia.gg@gmail.com>
2022-05-10 11:17:04 +0800

    added initialization of items data

--- database/databaseInit.js
+++ database/databaseInit.js
@@ -65,12 +65,140 @@ exports.insertGroups = async (req, res) => {
   }
 };
 
-exports.insertItems = async () => {
-  const items = {
-    GROUP_ID: [],
-  };
-
+exports.insertItems = async (req, res) => {
   try {
+    const items = [
+      {
+        ID: "",
+        NAME: "Logitech Ergonomic",
+        GROUP_ID: "2d4k951o3961ae5r63y4b",
+        UNIT_PRICE: Math.floor(Math.random() * 905001) + 5000,
+        DATE_ADDED: new Date(
+          2022,
+          Math.floor(Math.random() * 12),
+          Math.floor(Math.random() * 31) + 1
+        ),
+      },
+      {
+        ID: "",
+        NAME: "RAKK Ilis RGB Mechanical Keyboard",
+        GROUP_ID: "2d4k951o3961ae5r63y4b",
+        UNIT_PRICE: Math.floor(Math.random() * 905001) + 5000,
+        DATE_ADDED: new Date(
+          2022,
+          Math.floor(Math.random() * 12),
+          Math.floor(Math.random() * 31) + 1
+        ),
+      },
+
+      {
+        ID: "",
+        NAME: "A4Tech Wired Optical Mouse",
+        GROUP_ID: "3u165moo93usm41e69502",
+        UNIT_PRICE: Math.floor(Math.random() * 905001) + 5000,
+        DATE_ADDED: new Date(
+          2022,
+          Math.floor(Math.random() * 12),
+          Math.floor(Math.random() * 31) + 1
+        ),
+      },
+      {
+        ID: "",
+        NAME: "Razer Wireless Gaming RGB Mouse",
+        GROUP_ID: "3u165moo93usm41e69502",
+        UNIT_PRICE: Math.floor(Math.random() * 905001) + 5000,
+        DATE_ADDED: new Date(
+          2022,
+          Math.floor(Math.random() * 12),
+          Math.floor(Math.random() * 31) + 1
+        ),
+      },
+
+      {
+        ID: "",
+        NAME: 'Mi 1C 23" Monitor',
+        GROUP_ID: "m1o3o6ni946t315sr1952",
+        UNIT_PRICE: Math.floor(Math.random() * 905001) + 5000,
+        DATE_ADDED: new Date(
+          2022,
+          Math.floor(Math.random() * 12),
+          Math.floor(Math.random() * 31) + 1
+        ),
+      },
+      {
+        ID: "",
+        NAME: 'Asus 1Q412 22" Thin Bezzel Display',
+        GROUP_ID: "m1o3o6ni946t315sr1952",
+        UNIT_PRICE: Math.floor(Math.random() * 905001) + 5000,
+        DATE_ADDED: new Date(
+          2022,
+          Math.floor(Math.random() * 12),
+          Math.floor(Math.random() * 31) + 1
+        ),
+      },
+
+      {
+        ID: "",
+        NAME: "Samsung OLED Thin Curved TV",
+        GROUP_ID: "i51v2e63l9ie6ts915432",
+        UNIT_PRICE: Math.floor(Math.random() * 905001) + 5000,
+        DATE_ADDED: new Date(
+          2022,
+          Math.floor(Math.random() * 12),
+          Math.floor(Math.random() * 31) + 1
+        ),
+      },
+
+      {
+        ID: "",
+        NAME: "HP Personal Computer Desktop Set",
+        GROUP_ID: "sys95u2435336t1en1m69",
+        UNIT_PRICE: Math.floor(Math.random() * 905001) + 5000,
+        DATE_ADDED: new Date(
+          2022,
+          Math.floor(Math.random() * 12),
+          Math.floor(Math.random() * 31) + 1
+        ),
+      },
+      {
+        ID: "",
+        NAME: "Asus Gaming RGB Destop",
+        GROUP_ID: "sys95u2435336t1en1m69",
+        UNIT_PRICE: Math.floor(Math.random() * 905001) + 5000,
+        DATE_ADDED: new Date(
+          2022,
+          Math.floor(Math.random() * 12),
+          Math.floor(Math.random() * 31) + 1
+        ),
+      },
+    ];
+
+    // assign id to each item
+    for (let i = 0; i < items.length; i++) {
+      let tempId;
+
+      while (true) {
+        tempId = await generateKey(items[i].NAME);
+
+        const isPresent = await validateKey(tempId, "item_id", "item");
+        if (!isPresent) break;
+      }
+
+      items[i].ID = tempId;
+    }
+
+    // build sql params
+    let arrayParams = [];
+    items.forEach(({ ID, NAME, GROUP_ID, UNIT_PRICE, DATE_ADDED }) => {
+      arrayParams.push([ID, NAME, UNIT_PRICE, GROUP_ID, DATE_ADDED]);
+    });
+
+    const insertRes = await runMultipleQuery(
+      `INSERT INTO item(item_id, name, unit_price, group_id, date_added) VALUES(?, ?, ?, ?, ?)`,
+      arrayParams
+    );
+
+    return res.status(200).json({ insertRes });
   } catch (error) {
     return res
       .status(httpStatus.INTERNAL_SERVER_ERROR)
