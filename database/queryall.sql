d97c731ae15ddf6d536233ec76938b104fa4ecc6
gn-grc <genejogarcia.gg@gmail.com>
2022-06-03 13:24:28 +0800

    database updates

--- database/queryall.sql
+++ database/queryall.sql
@@ -1,5 +1,6 @@
--- SQLite
 select 
+    item.account_id,
+    username,
     item.item_id,
     name,
     item.group_id,
@@ -7,9 +8,9 @@ select
     inventory_id,
     quantity
 from item
-left join item_group 
+inner join account
+    on account.account_id = item.account_id
+inner join item_group 
     on item.group_id = item_group.group_id
 left join inventory
     on inventory.item_id = item.item_id;
-
-select * from inventory;
